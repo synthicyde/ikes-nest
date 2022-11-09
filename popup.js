408921ec0ced3242bd950cfcbc295481f21df41e
Synthicyde <35822328+synthicyde@users.noreply.github.com>
2022-11-09 14:17:17 -0500

    Links now pull from locat storage

--- popup.js
+++ popup.js
@@ -11,29 +11,25 @@ logo.addEventListener("click", async () => {
 });
 
 let userLinks = [];
-function createUserLinks() {
-	fetch("./userlinks.json").then((response) => {
-		return response.json();
-	}).then(data => {
-		userLinks = data;
-		for (let i = 0; i < userLinks.length; i++) {
-			const targetUserLinks = document.getElementById("user_links");
-			const li = document.createElement("li");
-			const a = document.createElement("a");
-			li.id = "user-link-" + i;
-			let targetDiv = "user-link-" + i;
-			a.id = "user-link-actual-" + i;
-			let targetLink = "user-link-actual-" + i;
-			targetUserLinks.appendChild(li.cloneNode(true));
-			let targetLi = document.getElementById(targetDiv);
-			targetLi.appendChild(a.cloneNode(true));
-			document.getElementById(targetLink).innerHTML = userLinks[i].ArticleName;
-			document.getElementById(targetLink).href = userLinks[i].ArticleURL;
-			document.getElementById(targetLink).target = userLinks[i].target;
-		};
-	}).catch((err) => {
-		console.log(err);
-	});
+function postUserLinks() {
+	for (let i = 0; i < userLinks.length; i++) {
+		const targetUserLinks = document.getElementById("user_links");
+		const li = document.createElement("li");
+		const a = document.createElement("a");
+		li.id = "user-link-" + i;
+		let targetDiv = "user-link-" + i;
+		a.id = "user-link-actual-" + i;
+		let targetLink = "user-link-actual-" + i;
+		targetUserLinks.appendChild(li.cloneNode(true));
+		let targetLi = document.getElementById(targetDiv);
+		targetLi.appendChild(a.cloneNode(true));
+		document.getElementById(targetLink).innerHTML = userLinks[i].ArticleName;
+		document.getElementById(targetLink).href = userLinks[i].ArticleURL;
+		document.getElementById(targetLink).target = userLinks[i].target;
+	};
 };
 
-createUserLinks();
+chrome.storage.sync.get(["storedUserLinks"], function(result) {
+	userLinks = result.storedUserLinks;
+	postUserLinks();
+});
