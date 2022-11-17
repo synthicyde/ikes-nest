a20af920999d5ee172f6f60a9e3fb66bc1cd77a2
synthicyde <buzzir1001@gmail.com>
2022-11-17 18:48:14 -0500

    Get links to load in options - clean CSS

--- options.js
+++ options.js
@@ -1,4 +1,24 @@
 const bookingsLink = document.getElementById("bookings_form");
+const inputText = document.createElement("input");
+const inputUrl = document.createElement("input");
+const br = document.createElement("br");
+const targetDiv = document.getElementById("link_container");
+
+function setUpLinks(number) {
+	inputText.id = "name_" + number;
+	inputText.type = "text";
+	inputUrl.id = "link_" + number;
+	inputUrl.type = "url";
+	br.setAttribute("class", "br_" + number);
+};
+
+function postLinks() {
+	targetDiv.appendChild(inputText.cloneNode(true));
+	targetDiv.appendChild(br.cloneNode(true));
+	targetDiv.appendChild(inputUrl.cloneNode(true));
+	targetDiv.appendChild(br.cloneNode(true));
+	targetDiv.appendChild(br.cloneNode(true));
+};
 
 bookingsLink.addEventListener("submit", (submission) => {
 	submission.preventDefault();
@@ -16,4 +36,16 @@ window.onload = () => {
 		console.log(userBookings);
 		document.getElementById("bookings").setAttribute("value", userBookings);
 	});
+	chrome.storage.sync.get(["storedUserLinks"], function(result) {
+		console.log(result.storedUserLinks);
+		let userLinks = result.storedUserLinks;
+		console.log(userLinks.length);
+		for (let i = 0; i < userLinks.length; i++) {
+			setUpLinks(i);
+			inputText.value = userLinks[i].ArticleName;
+			inputUrl.value = userLinks[i].ArticleURL;
+			postLinks();
+		}
+	});
 };
+
